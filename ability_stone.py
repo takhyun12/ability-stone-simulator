511abf0d0b730a320d23210f42075a368a82752f
takhyun12 <takhyun12@gmail.com>
2021-07-20 15:00:50 +0900

    어빌리티 스톤 객체 개선과 시뮬레이터 개발 중

--- ability_stone.py
+++ ability_stone.py
@@ -8,19 +8,23 @@ class Ability_Stone:
         """ Make a Stone Object """
         self.MAX_PROB = kwargs['MAX_PROB']
         self.MIN_PROB = kwargs['MIN_PROB']
-        self.current_prob: float = kwargs['MAX_PROB']
+        self.current_prob: float = self.MAX_PROB
         self.increase_ability_1 = list(['◇'] * kwargs['STONE_LEVEL'])
         self.increase_ability_2 = list(['◇'] * kwargs['STONE_LEVEL'])
         self.decrease_ability_1 = list(['◇'] * kwargs['STONE_LEVEL'])
 
-    def crafting(self, selected_ability: int, crafting_result: bool) -> None:
+    def crafting(self) -> bool:
+        """ Ability Stone Crafting """
+        return random.randrange(100) < int(self.current_prob * 100)
+
+    def simulation(self, selected_ability: int, crafting_result: bool) -> None:
         """
-        Ability Point Crafting
-        Status Code : (0: None, 1: Pass, 2: Fail)
+        simulation and get recommendations
+        Status Code : (◇: None, ◆: Pass, ✖: Fail)
         """
         # [1] Exception handle
         if selected_ability < 0 or selected_ability > 3:
-            raise Exception('Invalid number request')
+            raise Exception('Invalid number request!')
 
         # [2] Find Ability Table (O(1))
         ability_table: list = self.get_target_table(selected_ability=selected_ability)
@@ -29,30 +33,40 @@ class Ability_Stone:
         for index in range(len(ability_table)):
             if ability_table[index] == '◇':
                 ability_table[index] = '◆' if crafting_result else '✖'
-                self.calc_prob(crafting_result)
+                self.get_current_prob(selected_ability=selected_ability, crafting_result=crafting_result)
                 self.get_recommendation()
-
                 break
         else:
-            pass
+            raise Exception('request overflow!')
 
     def get_target_table(self, selected_ability: int) -> list:
-        """ Get target ability object """
+        """ Get target ability object (O(1)) """
         return self.increase_ability_1 if selected_ability == 1 else self.increase_ability_2 if selected_ability == 2 else self.decrease_ability_1
 
-    def calc_prob(self, crafting_result: bool) -> None:
-        """ Calc Probabilities based on user request """
-        if crafting_result:
-            if self.current_prob < self.MAX_PROB:  # Pass
-                self.current_prob += 0.15
-                self.current_prob = round(self.current_prob, 2)
-        else:
-            if self.current_prob > self.MIN_PROB:  # Fail
-                self.current_prob -= 0.15
-                self.current_prob = round(self.current_prob, 2)
+    def get_current_prob(self, selected_ability: int, crafting_result: bool) -> None:
+        """ Calc Probabilities based on user request (O(1)) """
+        if selected_ability != 3:  # Increase Ability
+            if crafting_result:  # Pass Event
+                if self.current_prob > self.MIN_PROB:
+                    self.current_prob -= 0.15
+                    self.current_prob = round(self.current_prob, 2)
+            else:  # Fail Event
+                if self.current_prob < self.MAX_PROB:
+                    self.current_prob += 0.15
+                    self.current_prob = round(self.current_prob, 2)
+
+        else:  # Decrease Ability
+            if crafting_result:  # Pass Event
+                if self.current_prob < self.MAX_PROB:
+                    self.current_prob += 0.15
+                    self.current_prob = round(self.current_prob, 2)
+            else:
+                if self.current_prob > self.MIN_PROB:
+                    self.current_prob -= 0.15
+                    self.current_prob = round(self.current_prob, 2)
 
     def get_recommendation(self) -> dict:
-        """ This function is under development """
+        """ This function is under development (O(1)) """
         return {'1': self.MAX_PROB, '2': 0.15, '3': 0.17}
 
     def show_status(self) -> None:
@@ -61,6 +75,7 @@ class Ability_Stone:
         print(f'Increase Ability 1 : {self.increase_ability_1}')
         print(f'Increase Ability 2 : {self.increase_ability_2}')
         print(f'Decrease Ability 1 : {self.decrease_ability_1}')
+        print('')
 
 
 if __name__ == '__main__':
@@ -68,15 +83,14 @@ if __name__ == '__main__':
 
     # Test
     for i in range(1, 4):
-        for j in range(0, 10):
-            r = bool(random.getrandbits(1))
-            ability_stone.crafting(selected_ability=i, crafting_result=r)
+        for j in range(0, 6):
+            ability_stone.simulation(selected_ability=i, crafting_result=ability_stone.crafting())
             ability_stone.show_status()
 
     c1 = Counter(ability_stone.increase_ability_1)
     c2 = Counter(ability_stone.increase_ability_2)
     c3 = Counter(ability_stone.decrease_ability_1)
 
-    print(c1)
-    print(c2)
-    print(c3)
+    print(sorted(c1.items(), key=lambda x: x[0]))
+    print(sorted(c2.items(), key=lambda x: x[0]))
+    print(sorted(c3.items(), key=lambda x: x[0]))
--- simulator.py
+++ simulator.py
@@ -1,18 +1,25 @@
 from ability_stone import Ability_Stone
+from collections import Counter
 
 if __name__ == '__main__':
     ability_stone = Ability_Stone(MAX_PROB=0.75, MIN_PROB=0.25, STONE_LEVEL=10)
-    ability_stone.show_status()
-
-    # Test
-    print('----')
-    for i in range(0, 12):
-        ability_stone.crafting(selected_number=1)
-
-    for i in range(0, 2):
-        ability_stone.crafting(selected_number=3)
-
-    for i in range(0, 4):
-        ability_stone.crafting(selected_number=2)
 
     ability_stone.show_status()
+    while True:
+        try:
+            ability_stone.simulation(selected_ability=int(input("돌을 선택하세요(1~3) : ")), crafting_result=ability_stone.crafting())
+            ability_stone.show_status()
+            if '◇' not in ability_stone.increase_ability_1 and '◇' not in ability_stone.increase_ability_2 and '◇' not in ability_stone.decrease_ability_1:
+                break
+        except Exception as ex:
+            print(ex)
+            print('')
+            pass
+
+    c1 = Counter(ability_stone.increase_ability_1)
+    c2 = Counter(ability_stone.increase_ability_2)
+    c3 = Counter(ability_stone.decrease_ability_1)
+
+    print(sorted(c1.items(), key=lambda x: x[0]))
+    print(sorted(c2.items(), key=lambda x: x[0]))
+    print(sorted(c3.items(), key=lambda x: x[0]))
