7f874af0bdacbb96ca3bcd9481ce094b217b7e5c
takhyun12 <takhyun12@gmail.com>
2021-07-28 12:50:54 +0900

    insert stream -> query로 변경

--- Backend/bq_insert_query_json.py
+++ Backend/bq_insert_query_json.py
@@ -1,18 +1,25 @@
 import os
 from google.cloud import bigquery
+from datetime import datetime
 
 credentials_path = 'Resource/bigquery_private_key.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 
 client = bigquery.Client()
-table_id = 'ability-stone-simulator.craft_log_dataset.test_table'
+table_id = 'ability-stone-simulator.test_dataset.payment_table'
+time_stamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-select_query = """
-    SELECT *
-    FROM `ability-stone-simulator.craft_log_dataset.test_table`
+'''
+insert_query = f"""
+    INSERT INTO `{table_id}`(customer_id, license_type, remaining_time, time_refill_date, time_refill_quantity, subscription_date, last_access_time)
+    VALUES ("qatester1@onthelive.kr", "Basic", 14400, "{time_stamp}", 14400, "{time_stamp}", "{time_stamp}")
 """
+'''
+insert_query = f"""
+    INSERT INTO `{table_id}`(payment_id, payment_date, next_payment_date, amount, currency, customer_id, product_id, order_id)
+    VALUES ("P2021072812063921291", "{time_stamp}", "2021-07-28", 3000.0, "KRW", "qatester1@onthelive.kr", "PR01", "Noiist Windows Application")
+"""
+print(insert_query)
 
-query_job = client.query(select_query)
+query_job = client.query(insert_query)
 query_job.result()
-for row in query_job:
-    print("name={}, age={}".format(row[0], row[1]))
--- Backend/bq_read_json.py
+++ Backend/bq_read_json.py
@@ -5,14 +5,27 @@ credentials_path = 'Resource/bigquery_private_key.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 
 client = bigquery.Client()
-table_id = 'ability-stone-simulator.test_dataset.test_table'
+customer_table_id = 'ability-stone-simulator.test_dataset.customer_table'
+payment_table_id = 'ability-stone-simulator.test_dataset.payment_table'
 
-select_query = """
-    SELECT *
-    FROM `ability-stone-simulator.test_dataset.test_table`
+select_query = f"""
+SELECT
+  customer.customer_id, customer.license_type, customer.remaining_time, customer.time_refill_date, customer.time_refill_quantity, 
+  payment.next_payment_date, payment.amount, payment.currency
+FROM
+  `{customer_table_id}` as customer
+INNER JOIN
+  `{payment_table_id}` as payment
+ON
+  customer.customer_id = payment.customer_id
+WHERE 
+  customer.customer_id = "qatester1@onthelive.kr"
+ORDER BY
+  order_id;
 """
 
 query_job = client.query(select_query)
 query_job.result()
+
 for row in query_job:
     print(row)
