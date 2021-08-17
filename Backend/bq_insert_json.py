2653b56f9ba3384acf483aa20dae6876928bd40f
takhyun12 <takhyun12@gmail.com>
2021-08-17 14:23:23 +0900

    일단 pub/sub 제거

--- Backend/bq_insert_json.py
+++ Backend/bq_insert_json.py
@@ -1,30 +1,72 @@
 import os
+import json
+import dataclasses
 from google.cloud import bigquery
+
+
+@dataclasses.dataclass
+class Font_Data:
+    font_id: str
+    language: str
+    name: str
+    version: str
+    preferences: str
+    main: str
+    about: str
+    start: str
+
 
 credentials_path = 'Resource/bigquery_private_key.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 
 client = bigquery.Client()
-table_id = 'ability-stone-simulator.test_dataset.test_table'
+table_id = 'ability-stone-simulator.resource.font_table'
 
-row_to_insert = [{
-    "customer_id": "admin@noiist.ai",
-    "license_type": "Admin",
-    "remaining_time": 6048000,
-    "time_refill_date": "2021-07-26 18:14:28.480028",
-    "time_refill_quantity": 6048000,
-    "product_id": "",
-    "product_name": "",
-    "payment_id": "",
-    "payment_date": "1900-01-01",
-    "next_payment_date": "1900-01-01",
-    "amount": 0.0,
-    "currency": "",
-    "order_id": "",
-    "order_date": "1900-01-01"
+row = [{
+    "font_id": "FP01",
+    "language": "English",
+    "name": "English Font Pack",
+    "version": "1.0.0.0",
+    "main": {
+        "AccentButton": "Poppins, 14, bold",
+        "ToggleButton": "Poppins, 9",
+        "Switch": "Poppins, 12",
+        "TCheckbutton": "Poppins, 11",
+        "TCombobox": "Noto Sans CJK KR Medium (TTF), 12",
+        "menu": "Poppins, 12",
+        "user_id_label": "Poppins, 12",
+        "start_label": "Poppins, 16, bold",
+        "microphone_label": "Poppins, 16",
+        "input_device_combo": "Noto Sans CJK KR Medium (TTF), 12",
+        "speaker_label": "Poppins, 16",
+        "output_device_combo": "Noto Sans CJK KR Medium (TTF), 12",
+        "license_message": "Poppins, 13",
+        "toggle_button": {
+            "left_button": "Poppins, 9, bold",
+            "right_button": "Poppins, 9, bold"
+        }
+    },
+    "preferences": {
+        "title_label": "Poppins, 16, bold",
+        "language_label": "Poppins, 14, bold",
+        "language_combo": "Noto Sans CJK KR Medium (TTF), 12",
+        "advanced_label": "Poppins, 14, bold",
+        "notice_label": "Poppins, 10"
+    },
+    "about": {
+        "software_name_label": "Poppins, 16, bold",
+        "version_label": "Poppins, 10",
+        "build_label": "Poppins, 10",
+        "contents_message": "Poppins, 10",
+        "copyrights_label": "Poppins, 10"
+    },
+    "start": {
+        "content_message": "Poppins, 10",
+        "start_button": "Poppins, 12, bold"
+    }
 }]
 
-errors = client.insert_rows_json(table_id, row_to_insert)
+errors = client.insert_rows_json(table_id, font_data)
 if not errors:
     print('New rows have been added.')
 else:
--- simulator.py
+++ simulator.py
@@ -2,25 +2,10 @@ import os
 import json
 from collections import Counter
 from datetime import datetime
-from google.cloud import pubsub_v1
 from ability_stone import Ability_Stone
-
-TOPIC_PATH = 'projects/ability-stone-simulator/topics/logging-topic'
-CREDENTIALS_PATH = 'Resource/pubsub_private_key.json'
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = CREDENTIALS_PATH
-
-
-class PubFunction:
-    def send_craft_log(**self: dict) -> None:
-        json_data = json.dumps(self, indent=4, ensure_ascii=False)
-        data = json_data.encode('utf-8')
-        print(data)
-        publisher.publish(TOPIC_PATH, data)
 
 
 if __name__ == '__main__':
-    publisher = pubsub_v1.PublisherClient()
-
     ability_stone = Ability_Stone(MAX_PROB=0.75, MIN_PROB=0.25, STONE_LEVEL=10)
     ability_stone.show_status()
     while True:
@@ -34,14 +19,6 @@ if __name__ == '__main__':
             c1 = Counter(ability_stone.increase_ability_1)
             c2 = Counter(ability_stone.increase_ability_2)
             c3 = Counter(ability_stone.decrease_ability_1)
-
-            PubFunction.send_craft_log(time_stamp=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                       selected_ability=str(selected_ability),
-                                       crafting_result=str(crafting_result),
-                                       current_prob=str(ability_stone.current_prob),
-                                       increase_ability_1=str(sorted(c1.items())),
-                                       increase_ability_2=str(sorted(c2.items())),
-                                       decrease_ability_1=str(sorted(c3.items())))
 
             if '◇' not in ability_stone.increase_ability_1 and '◇' not in ability_stone.increase_ability_2 and '◇' not in ability_stone.decrease_ability_1:
                 break
